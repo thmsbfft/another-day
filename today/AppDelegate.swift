6ea18182a9a2fe0335544c819d8783d9a95ebb3b
thmsbfft <thmsbfft@gmail.com>
2018-08-08 09:59:44 -0700

    Initial Commit

---
+++ today/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  today
+//
+//  Created by thmsbfft on 8/8/18.
+//  Copyright © 2018 thmsbfft. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
