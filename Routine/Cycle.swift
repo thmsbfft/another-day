d88ff38e09fbd648f3cdaeaa16aefb07bcd9be20
thmsbfft <thmsbfft@gmail.com>
2018-08-12 13:33:39 -0700

    start setup cycle

---
+++ Routine/Cycle.swift
@@ -0,0 +1,25 @@
+//
+//  Cycle.swift
+//  Routine
+//
+//  Created by thmsbfft on 8/12/18.
+//  Copyright © 2018 thmsbfft. All rights reserved.
+//
+
+import Foundation
+
+class Cycle {
+    
+    static func shouldDoCycle() {
+        
+    }
+    
+    static func doCycle() {
+        
+    }
+    
+    static func backup() {
+        
+    }
+    
+}
